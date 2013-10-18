d288dfcbd3246d968464e950bcaaac0b5ed1155b
Jeremy Shipman <jeremy@burnbright.net>
2013-10-18 14:08:02 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Has one field
+
+Allows you to create a CMS button for creating and editing a single related object.
+It is a grid field, but just looks like a button.
+
+Very much a proof-of-concept at this stage. Feel free to take this and make it nice :smirk:
+
+## How to use
+
+You must pass through the parent context, so that the has_one relationship can be set
+by the `GridFieldDetailForm`.
+
+```php
+	function getCMSFields(){
+		$fields = parent::getCMSFields();
+		$fields->merge(new FieldList(
+			HasOneButtonField::create("Likebox", "Likebox", $this->Likebox(), $this)
+		));
+		return $fields;
+	}
+```
---
+++ code/HasOneButtonField.php
@@ -0,0 +1,70 @@
+<?php
+
+class HasOneButtonField extends GridField{
+
+	protected $record;
+	protected $parent;
+
+	function __construct($name, $title = null, $record, $parent){
+		$this->record = $record;
+		$this->parent = $parent;
+		$config = GridFieldConfig::create()
+					->addComponent(new GridFieldDetailForm())
+					->addComponent(new GridFieldHasOneEditButton());			
+		$list = new HasOneButtonRelationList($this->record, $name, $parent);
+		parent::__construct($name, $title, $list, $config);
+	}
+
+	function getRecord(){
+		return $this->record;
+	}
+
+}
+
+class GridFieldHasOneEditButton extends GridFieldAddNewButton implements GridField_HTMLProvider {
+
+	public function getHTMLFragments($gridField) {
+		$record = $gridField->getRecord();
+		if(!$record->exists() || !$record->isInDB()){
+			return parent::getHTMLFragments($gridField); //use parent add button
+		}
+		$singleton = singleton($gridField->getModelClass());
+		if(!$singleton->canCreate()) {
+			return array();
+		}
+		if(!$this->buttonName) {
+			// provide a default button name, can be changed by calling {@link setButtonName()} on this component
+			$objectName = $singleton->i18n_singular_name();
+			$this->buttonName = _t('GridField.Edit', 'Edit {name}', array('name' => $objectName));
+		}
+		$data = new ArrayData(array(
+			'NewLink' => Controller::join_links($gridField->Link('item'), $record->ID, 'edit'),
+			'ButtonName' => $this->buttonName,
+		));
+
+		return array(
+			$this->targetFragment => $data->renderWith('GridFieldAddNewbutton')
+		);
+	}
+
+}
+
+class HasOneButtonRelationList extends DataList{
+
+	protected $record;
+	protected $name;
+	protected $parent;
+
+	function __construct($record, $name, $parent){
+		$this->record = $record;
+		$this->name = $name;
+		$this->parent = $parent;
+		parent::__construct($record->ClassName);
+	}
+
+	function add($item){
+		$this->parent->{$this->name."ID"} = $item->ID;
+		$this->parent->write();
+	}
+
+}
