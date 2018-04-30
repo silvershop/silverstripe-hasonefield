a414a7e0efb6bddf5e67be1857688fd11685418d
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-04-30 22:05:53 +0100

    Setup link existing and unlink buttons

--- src/HasOneButtonField.php
+++ src/HasOneButtonField.php
@@ -5,6 +5,7 @@ namespace SilverShop\HasOneField;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldConfig;
 use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
 
 /**
  * Class HasOneButtonField
@@ -20,9 +21,20 @@ class HasOneButtonField extends GridField
         $this->record = $parent->{$name}();
         $this->parent = $parent;
         $config = GridFieldConfig::create()
-                    ->addComponent(new GridFieldDetailForm())
-                    ->addComponent(new GridFieldHasOneEditButton());
+            ->addComponent(new GridFieldSummaryField($name))
+            ->addComponent(new GridFieldDetailForm())
+            ->addComponent(new GridFieldHasOneEditButton())
+            ->addComponent(new GridFieldHasOneUnlinkButton($parent));
+
+        if (!$this->record->exists()) {
+            $config->addComponent(new GridFieldAddExistingAutocompleter());
+        }
+
         $list = new HasOneButtonRelationList($this->record, $name, $parent);
+
+        // Limit the existing list so that autocomplete will find results
+        $list = $list->filter("ID", $this->record->ID);
+
         parent::__construct($name, $title, $list, $config);
     }
 
