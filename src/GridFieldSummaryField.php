44c3af48c3bad7adc890c928e47060560b055517
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-07-30 11:00:02 +1200

    Refactor summary field component

--- src/GridFieldSummaryField.php
+++ src/GridFieldSummaryField.php
@@ -2,10 +2,8 @@
 
 namespace SilverShop\HasOneField;
 
-use SilverStripe\View\ArrayData;
-use SilverStripe\Forms\TextField;
-use SilverStripe\Control\Controller;
-use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Core\Convert;
+use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridField_HTMLProvider;
 use SilverStripe\Forms\ReadonlyField;
 
@@ -18,43 +16,59 @@ class GridFieldSummaryField implements GridField_HTMLProvider
     /**
      * The name of the relation
      *
-     * @var string 
+     * @var string
      */
-    protected $name;
+    protected $relationName;
+    /**
+     * The location of this fragmemt
+     *
+     * @var string
+     */
+    protected $targetFragment;
+    /**
+     * @var string
+     */
+    protected $summaryField;
+
+    /**
+     * Setup this component
+     *
+     * @param string $name The name of the relation
+     * @param string $summaryField The field on the record to use for the summary
+     * @param string $targetFragment The location of this fragment
+     */
+    public function __construct($name, $summaryField = "Title", $targetFragment = 'before')
+    {
+        $this->relationName = $name;
+        $this->targetFragment = $targetFragment;
+        $this->summaryField = $summaryField;
+    }
 
     /**
      * Get the value of name
      *
      * @return string
-     */ 
-    public function getName()
+     */
+    public function getRelationName()
     {
-        return $this->name;
+        return $this->relationName;
     }
 
     /**
      * Set the value of name
      *
-     * @param string $name the relation name
-     *
-     * @return self
-     */ 
-    public function setName(string $name)
+     * @param string $relationName the relation name
+     * @return static
+     */
+    public function setRelationName($relationName)
     {
-        $this->name = $name;
+        $this->relationName = $relationName;
         return $this;
     }
 
     /**
-     * The location of this fragmemt
-     * 
-     * @var string
-     */
-    protected $targetFragment;
-
-    /**
      * Get the value of targetFragment
-     */ 
+     */
     public function getTargetFragment()
     {
         return $this->targetFragment;
@@ -63,10 +77,10 @@ class GridFieldSummaryField implements GridField_HTMLProvider
     /**
      * Set the value of targetFragment
      *
-     * @param string $targetFragment The target fragment 
-     * 
+     * @param string $targetFragment The target fragment
+     *
      * @return self
-     */ 
+     */
     public function setTargetFragment($targetFragment)
     {
         $this->targetFragment = $targetFragment;
@@ -74,15 +88,10 @@ class GridFieldSummaryField implements GridField_HTMLProvider
     }
 
     /**
-     * @var string
-     */
-    protected $summaryField;
-
-    /**
      * Get the value of summaryField
      *
      * @return string
-     */ 
+     */
     public function getSummaryField()
     {
         return $this->summaryField;
@@ -92,51 +101,31 @@ class GridFieldSummaryField implements GridField_HTMLProvider
      * Set the value of summaryField
      *
      * @param string $summaryField The field name
-     *
-     * @return self
-     */ 
-    public function setSummaryField(string $summaryField)
+     * @return static
+     */
+    public function setSummaryField($summaryField)
     {
         $this->summaryField = $summaryField;
         return $this;
     }
 
     /**
-     * Setup this component
-     *
-     * @param string $name           The name of the relation
-     * @param string $summaryField   The field on the record to use for the summary 
-     * @param string $targetFragment The location of this fragment
-     */
-    public function __construct($name, $summaryField = "Title", $targetFragment = 'before')
-    {
-        $this->name = $name;
-        $this->targetFragment = $targetFragment;
-        $this->summaryField = $summaryField;
-    }
-
-    /**
      * Generate HTML for the GridField
-     * 
-     * @param GridField $gridField The current GridField
-     * 
+     *
+     * @param GridField|HasOneButtonField $gridField The current GridField
      * @return array
      */
     public function getHTMLFragments($gridField)
     {
         $record = $gridField->getRecord();
-        $summary = $this->summaryField;
-        $name = $this->name;
 
         $field = ReadonlyField::create(
-            'gridfield_hasone_summary',
-            $name
-        );
-        
-        $field->setValue($record->{$summary});
+            $gridField->getName() . '_' . Convert::raw2htmlid(static::class),
+            ReadonlyField::name_to_label($this->relationName)
+        )->setValue($record->getField($this->summaryField));
 
         return [
-            $this->targetFragment => $field->FieldHolder()
+            $this->targetFragment => $field->FieldHolder(),
         ];
     }
 }
