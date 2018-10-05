9aa59cc0b0452cc10f6c527cf1a5162823ff4178
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-10-05 15:11:53 +1300

    Convert HasOneButtonRelationList to an ArrayList
    Stops re-querying database to get the same object back.

--- src/HasOneButtonRelationList.php
+++ src/HasOneButtonRelationList.php
@@ -2,13 +2,13 @@
 
 namespace SilverShop\HasOneField;
 
-use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataObject;
 
 /**
  * Class HasOneButtonRelationList
  */
-class HasOneButtonRelationList extends DataList
+class HasOneButtonRelationList extends ArrayList
 {
     /**
      * @var DataObject
@@ -37,18 +37,22 @@ class HasOneButtonRelationList extends DataList
         $this->relationName = $relationName;
         $this->parent = $parent;
 
-        parent::__construct($record->ClassName);
+        parent::__construct([$record]);
     }
 
     public function add($item)
     {
         $this->parent->setField("{$this->relationName}ID", $item->ID);
         $this->parent->write();
+
+        $this->items = [$item];
     }
 
     public function remove($item)
     {
         $this->parent->setField("{$this->relationName}ID", 0);
         $this->parent->write();
+
+        $this->items = [];
     }
 }
