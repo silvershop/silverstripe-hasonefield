5d4a55dc13e6df139ba3ba2440f35baf8078be35
Lee Bradley <lee.bradley@mademedia.co.uk>
2018-10-17 11:59:03 +0100

    Fix for Issue #21: Auto complete field throws error
    Auto complete field throws error #21
    
    Fixes the search functionality but not the unlinking

--- src/HasOneAddExistingAutoCompleter.php
+++ src/HasOneAddExistingAutoCompleter.php
@@ -9,7 +9,10 @@ use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 use SilverStripe\View\ArrayData;
 use SilverStripe\View\SSViewer;
 use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
-use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\{ DataList, DataObject };
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Convert;
+use SilverStripe\Control\HTTPResponse;
 
 /**
  * Class GridFieldHasOneEditButton
@@ -18,9 +21,9 @@ class HasOneAddExistingAutoCompleter extends GridFieldAddExistingAutocompleter
 {
     /**
      * Check if a record has been set, if so, don't load the fields
-     * 
+     *
      * @param GridField $gridField
-     * 
+     *
      * @return array
      */
     public function getHTMLFragments($gridField)
@@ -75,5 +78,76 @@ class HasOneAddExistingAutoCompleter extends GridFieldAddExistingAutocompleter
             );
         }
     }
-    
+
+    /**
+     * Returns a json array of a search results that can be used by for example Jquery.ui.autosuggestion
+     *
+     * @param GridField $gridField
+     * @param HTTPRequest $request
+     * @return string
+     */
+    public function doSearch($gridField, $request)
+    {
+        $dataClass = $gridField->getModelClass();
+
+        $allList = $this->searchList ? $this->searchList : DataList::create($dataClass);
+
+        $searchFields = ($this->getSearchFields())
+            ? $this->getSearchFields()
+            : $this->scaffoldSearchFields($dataClass);
+
+        if (!$searchFields) {
+            throw new LogicException(
+                sprintf(
+                    'HasOneAddExistingAutoCompleter: No searchable fields could be found for class "%s"',
+                    $dataClass
+                )
+            );
+        }
+
+        $params = array();
+
+        foreach ($searchFields as $searchField) {
+            $name = (strpos($searchField, ':') !== false) ? $searchField : "$searchField:StartsWith";
+            $params[$name] = $request->getVar('gridfield_relationsearch');
+        }
+
+        $results = $allList
+            ->filterAny($params)
+            ->sort(strtok($searchFields[0], ':'), 'ASC')
+            ->limit($this->getResultsLimit())
+            ->toArray();
+
+        $savedList = $gridField->getList();
+
+        foreach ($results as $i=>$result) {
+            if ($savedList->find('ID', $result->ID)) {
+                unset($results[$i]);
+            }
+        }
+
+        $json = array();
+
+        Config::nest();
+
+        SSViewer::config()->update('source_file_comments', false);
+
+        $viewer = SSViewer::fromString($this->resultsFormat);
+
+        foreach ($results as $result) {
+            $title = Convert::html2raw($viewer->process($result));
+
+            $json[] = array(
+                'label' => $title,
+                'value' => $title,
+                'id' => $result->ID,
+            );
+        }
+
+        Config::unnest();
+
+        return HTTPResponse::create()
+            ->setBody(Convert::array2json($json))
+            ->addHeader('Content-Type', 'text/json');
+    }
 }
